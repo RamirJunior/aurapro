b1a2ded754db7ade57817b080c7972ba0a62e84c
Ramir Junior <junior.ribeiro0@live.com>
2025-07-31 11:25:09 -0300

    Initial commit

---
+++ src/main/java/br/gov/ma/aurapro/AuraproApplication.java
@@ -0,0 +1,13 @@
+package br.gov.ma.aurapro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuraproApplication.class, args);
+	}
+
+}
